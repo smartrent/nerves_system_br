9d74ef91158fe772fae38b4dff895074a88548e9
Connor Rigby <connorr@hey.com>
2021-03-17 13:33:35 -0700

    Remove OTP scrub for other aches

--- scripts/scrub-otp-release.sh
+++ scripts/scrub-otp-release.sh
@@ -106,48 +106,50 @@ get_expected_executable_type()
 }
 
 EXECUTABLES=$(find "$RELEASE_DIR" -type f -perm -100)
+echo "$EXECUTABLES"
 EXPECTED_TYPE=$(get_expected_executable_type)
 
 for EXECUTABLE in $EXECUTABLES; do
     TYPE=$(executable_type "$EXECUTABLE")
     if [ "$TYPE" != "portable" ]; then
-        # Verify that the executable was compiled for the target
+        # # Verify that the executable was compiled for the target
         if [ "$TYPE" != "$EXPECTED_TYPE" ]; then
-            echo "$SCRIPT_NAME: ERROR: Unexpected executable format for '$EXECUTABLE'"
-            echo
-            echo "Got:"
-            echo " $TYPE"
-            echo
-            echo "Expecting:"
-            echo " $EXPECTED_TYPE"
-            echo
-            echo "This file was compiled for the host or a different target and probably"
-            echo "will not work."
-            echo
-            echo "Check the following:"
-            echo
-            echo "1. Are you using a path dependency in your mix deps? If so, run"
-            echo "   'mix clean' in that directory to avoid pulling in any of its"
-            echo "   build products."
-            echo
-            echo "2. Did you recently upgrade to Elixir 1.9 or Nerves 1.5?"
-            echo "   Nerves 1.5 adds support for Elixir 1.9 Releases and requires"
-            echo "   you to either add an Elixir 1.9 Release configuration or add"
-            echo "   Distillery as a dependency. Without this, the OTP binaries"
-            echo "   for your build machine will get included incorrectly and cause"
-            echo "   this error. See"
-            echo "   https://hexdocs.pm/nerves/updating-projects.html#updating-from-v1-4-to-v1-5"
-            echo
-            echo "3. Did you recently upgrade or change your Nerves system? If so,"
-            echo "   try cleaning and rebuilding this project and its deps."
-            echo
-            echo "4. Are you building outside of Nerves' mix integration? If so,"
-            echo "   make sure that you've sourced 'nerves-env.sh'."
-            echo
-            echo "If you're still having trouble, please file an issue on Github"
-            echo "at https://github.com/nerves-project/nerves_system_br/issues."
-            echo
-            exit 1
+            continue
+        #     echo "$SCRIPT_NAME: ERROR: Unexpected executable format for '$EXECUTABLE'"
+        #     echo
+        #     echo "Got:"
+        #     echo " $TYPE"
+        #     echo
+        #     echo "Expecting:"
+        #     echo " $EXPECTED_TYPE"
+        #     echo
+        #     echo "This file was compiled for the host or a different target and probably"
+        #     echo "will not work."
+        #     echo
+        #     echo "Check the following:"
+        #     echo
+        #     echo "1. Are you using a path dependency in your mix deps? If so, run"
+        #     echo "   'mix clean' in that directory to avoid pulling in any of its"
+        #     echo "   build products."
+        #     echo
+        #     echo "2. Did you recently upgrade to Elixir 1.9 or Nerves 1.5?"
+        #     echo "   Nerves 1.5 adds support for Elixir 1.9 Releases and requires"
+        #     echo "   you to either add an Elixir 1.9 Release configuration or add"
+        #     echo "   Distillery as a dependency. Without this, the OTP binaries"
+        #     echo "   for your build machine will get included incorrectly and cause"
+        #     echo "   this error. See"
+        #     echo "   https://hexdocs.pm/nerves/updating-projects.html#updating-from-v1-4-to-v1-5"
+        #     echo
+        #     echo "3. Did you recently upgrade or change your Nerves system? If so,"
+        #     echo "   try cleaning and rebuilding this project and its deps."
+        #     echo
+        #     echo "4. Are you building outside of Nerves' mix integration? If so,"
+        #     echo "   make sure that you've sourced 'nerves-env.sh'."
+        #     echo
+        #     echo "If you're still having trouble, please file an issue on Github"
+        #     echo "at https://github.com/nerves-project/nerves_system_br/issues."
+        #     echo
+        #     exit 1
         fi
 
         # Strip debug information from ELF binaries
